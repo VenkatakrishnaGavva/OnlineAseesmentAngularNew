1e551d768585676a0f1e8f0dd2a7f97d94b7297e
VenkatakrishnaGavva <vas.478@gmail.com>
2018-02-25 10:18:55 +0530

    Modifying cmd text

--- deploy.cmd
+++ deploy.cmd
@@ -2,7 +2,7 @@
 
 :: ----------------------
 :: KUDU Deployment Script
-:: Version: 1.0.9
+:: Version: 1.0.16
 :: ----------------------
 
 :: Prerequisites
@@ -84,13 +84,13 @@ goto :EOF
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Deployment
 :: ----------
-
+ 
 :Deployment
-echo Handling angular-cli deployment.
-
+echo Handling node.js deployment.
+ 
 :: 1. Select node version
 call :SelectNodeVersion
-
+ 
 :: 2. Install npm packages
 IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   pushd "%DEPLOYMENT_SOURCE%"
@@ -98,18 +98,28 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
-
-:: 3. Build application
-IF EXIST "%DEPLOYMENT_SOURCE%\angular-cli.json" (
+ 
+:: 3. Angular Prod Build
+IF EXIST "%DEPLOYMENT_SOURCE%/.angular-cli.json" (
+echo Building App in %DEPLOYMENT_SOURCE%…
+pushd "%DEPLOYMENT_SOURCE%"
+call :ExecuteCmd !NPM_CMD! run build
+IF !ERRORLEVEL! NEQ 0 goto error
+popd
+)
+ 
+:: 4. Copy Web.config
+IF EXIST "%DEPLOYMENT_SOURCE%\web.config" (
   pushd "%DEPLOYMENT_SOURCE%"
-  call :ExecuteCmd node_modules\.bin\ng build --progress false --prod
+ :: the next line is optional to fix 404 error see section #8
+  call :ExecuteCmd cp web.config dist\
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
-
-:: 4. KuduSync
+ 
+:: 5. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;node_modules"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
