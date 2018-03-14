37027433dda38b7f27a86deca6874bfe9e896bfd
VenkatakrishnaGavva <vas.478@gmail.com>
2018-03-14 21:36:16 +0530

    Modifying cmd text

--- src/app/examresult/examresult.component.ts
+++ src/app/examresult/examresult.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./examresult.component.css']
 })
 export class ExamresultComponent implements OnInit {
+  rightAnsweredCount:string;
+  constructor() {
+    this.rightAnsweredCount = sessionStorage.getItem("rightAnsweredCount");
 
-  constructor() { }
+   }
 
   ngOnInit() {
   }
