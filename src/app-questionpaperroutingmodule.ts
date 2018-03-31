b8f9bcc9dd22be675ae353ed9da07ecd6bbf90de
VenkatakrishnaGavva <vas.478@gmail.com>
2018-03-31 13:32:40 +0530

    Login

--- src/app-questionpaperroutingmodule.ts
+++ src/app-questionpaperroutingmodule.ts
@@ -9,8 +9,8 @@ import { AuthGuardService as AuthGuard } from './app/auth/auth-guard.service';
 import { LoginComponent } from './app/login/login.component';
 
 const routes: Route[] = [
-  { path: '', component: QuestionPaperComponent,canActivate:[AuthGuard] },
     { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent,canActivate:[AuthGuard] },
   { path: 'QuestionPaper', component: QuestionPaperComponent,canActivate:[AuthGuard] },
   {path:'ExamResult',component:ExamresultComponent,canActivate:[AuthGuard]},
   { path: 'QuestionPaperUpload', component: QuestionpaperuploadComponent,canActivate:[AuthGuard] }
